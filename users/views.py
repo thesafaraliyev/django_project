932546b2bf2cac1aebb2bf92f902b451ad94949c
elshan <elshansafaraliyev@gmail.com>
2019-12-25 10:16:18 +0400

    user profile update

--- users/views.py
+++ users/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from .forms import UserRegisterForm
+from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
 
 
 def registration(request):
@@ -11,7 +11,7 @@ def registration(request):
             form.save()
             # username = form.cleaned_data.get('username')
 
-            messages.success(request, 'Your account has been created! You are now able to log in')
+            messages.success(request, 'Your account has been created! You are now able to log in.')
             return redirect('login')
 
     else:
@@ -21,4 +21,24 @@ def registration(request):
 
 @login_required
 def profile(request):
-    return render(request, 'users/profile.html')
+    if request.method == 'POST':
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        p_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
+
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+
+            messages.success(request, 'Your profile successfully updated.')
+            return redirect('profile')
+
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm(instance=request.user.profile)
+
+    data = {
+        'u_form': u_form,
+        'p_form': p_form,
+    }
+
+    return render(request, 'users/profile.html', data)
